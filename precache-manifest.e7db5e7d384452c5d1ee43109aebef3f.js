3390f9fe75599ed4d460e50b85e991b76e37db25
Yagnavalk Chaapala <31382160+yvdevy@users.noreply.github.com>
2020-09-20 08:54:03 +0530

    Updates

---
+++ precache-manifest.e7db5e7d384452c5d1ee43109aebef3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcf3c651eb7ade4edaabf521b3a18be3",
+    "url": "/IColor/index.html"
+  },
+  {
+    "revision": "555ab0855559051c619d",
+    "url": "/IColor/static/css/main.bf21bf3b.chunk.css"
+  },
+  {
+    "revision": "c3bbd380fd0b512fc058",
+    "url": "/IColor/static/js/2.543753dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/IColor/static/js/2.543753dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "555ab0855559051c619d",
+    "url": "/IColor/static/js/main.e2783788.chunk.js"
+  },
+  {
+    "revision": "9eefe89b840dd1f95a7e",
+    "url": "/IColor/static/js/runtime-main.41a0bca0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/IColor/static/media/logo.5d5d9eef.svg"
+  }
+]);
